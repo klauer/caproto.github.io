3f62f666c4bb02c88af33b4b330a58d8e5bf8184
K Lauer <klauer@slac.stanford.edu>
2020-08-01 11:46:38 -0700

    DOC: rebuilt with versions menu

--- caproto/v0.2.3/_static/documentation_options.js
+++ caproto/v0.2.3/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.3+0.g4b3b30e5.dirty',
+    VERSION: '0.2.3',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
